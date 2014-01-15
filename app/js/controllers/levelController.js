0726a29d8a4347d89cda0609566665d230e2676c
vkozulya <vkozulya@apostol.com>
2014-01-15 13:13:38 +0400

    random map

--- app/js/controllers/levelController.js
+++ app/js/controllers/levelController.js
@@ -1,14 +1,22 @@
 app.controller('LevelController', ['$scope', function ($scope) {
   console.log('LevelController init');
 
+  function makeRandomFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function makeNoiseMap(length) {
+    var map = new Array(length);
+
+    for (var i = 0; i < length; ++i)
+	  map[i] = makeRandomFromRange(0, 1);
+
+	return map;
+  }
+
   var level = {
     width: 5,
-	map: [
-      1, 1, 0, 0, 1,
-	  0, 0, 0, 0, 0,
-	  0, 1, 0, 0, 0,
-	  0, 0, 0, 0, 0
-	]
+	map: makeNoiseMap(20)
   }; 
 
   $scope.layers = ['back', 'mid', 'front', 'before'];
